af9bb72a75a39886493074eded213b831f2137a3
Ruben Täpptorp <ruben.tapptorp.6406@student.lu.se>
2024-01-24 12:47:59 +0100

    Fixat knapp med accelerometer

--- app/src/main/java/com/example/hellosensor/MainActivity.java
+++ app/src/main/java/com/example/hellosensor/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.hellosensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.hardware.Sensor;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, View.OnClickListener {
+
+    private TextView textView;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(this);
+
+        textView = findViewById(R.id.text_accelerometer);
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        textView.setText(sensorEvent.values[0]+"\n"+sensorEvent.values[1]+"\n"+sensorEvent.values[2]);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                Toast.makeText(this,"Button 1 clicked", Toast.LENGTH_SHORT).show();
+
+                sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+                sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                sensorManager.registerListener(MainActivity.this,sensor,sensorManager.SENSOR_DELAY_NORMAL);
+                break;
+        }
+
     }
 }
